a1629655a4891f03ba826806e0b50b909d5cc517
gzurakowski <gonchizurak@gmail.com>
2021-11-06 21:06:14 -0300

    Se crea la app

---
+++ main.js
@@ -0,0 +1,24 @@
+const express = require('express')
+    //const multer = require('multer')
+const app = express()
+
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static('public'))
+app.use(express.json())
+
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html')
+})
+
+
+
+
+/* ------------------------------------------------------ */
+/* Server Listen */
+const PORT = 8080
+const server = app.listen(PORT, () => {
+    console.log(`Servidor escuchando en el puerto ${server.address().port}`)
+})
+server.on('error', error => console.log(`Error en servidor error`))
